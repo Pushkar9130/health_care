eb78feedcfe73cb5ae264006b39cc73494f9a1ce
Pushkar9130 <pushkargodse9130@gmail.com>
2025-07-09 12:25:59 +0530

    Add files via upload

---
+++ doctors.js
@@ -0,0 +1,18 @@
+const doctors = [
+  { name: "Dr. Naresh Trehan", specialization: "Cardiothoracic Surgery", experience: "40+ years", contact: "naresh.trehan@medanta.com", photo: "images/naresh_trehan.jpg" },
+  { name: "Dr. Devi Prasad Shetty", specialization: "Pediatric Cardiac Surgery", experience: "35+ years", contact: "devi.shetty@narayanahealth.org", photo: "images/devi_shetty.jpg" },
+  { name: "Dr. Ashok Seth", specialization: "Interventional Cardiology", experience: "45+ years", contact: "ashok.seth@fortis.com", photo: "images/ashok_seth.jpg" },
+  { name: "Dr. Ramakanta Panda", specialization: "Cardiac Surgery", experience: "30+ years", contact: "ramakanta.panda@asianheart.in", photo: "images/ramakanta_panda.jpg" },
+  { name: "Dr. Randeep Guleria", specialization: "Pulmonology & Critical Care", experience: "30+ years", contact: "randeep.guleria@aiims.ac.in", photo: "images/randeep_guleria.jpg" },
+  { name: "Dr. Mammen Chandy", specialization: "Hematology", experience: "35+ years", contact: "mammen.chandy@tmc.gov.in", photo: "images/mammen_chandy.jpg" },
+  { name: "Dr. Siddhartha Mukherjee", specialization: "Oncology", experience: "20+ years", contact: "siddhartha@columbia.edu", photo: "images/siddhartha_mukherjee.jpg" },
+  { name: "Dr. Indira Hinduja", specialization: "Infertility & IVF", experience: "45+ years", contact: "indira.hinduja@lilavati.com", photo: "images/indira_hinduja.jpg" },
+  { name: "Dr. K. M. Cherian", specialization: "Cardiothoracic Surgery", experience: "50+ years", contact: "km.cherian@mmm.org", photo: "images/km_cherian.jpg" },
+  { name: "Dr. Mohamed Rela", specialization: "Liver Transplant Surgery", experience: "30+ years", contact: "mohamed.rela@chennaiuhs.in", photo: "images/mohamed_rela.jpg" },
+  // ... total 50 doctors; yahan se aap data copy karke add karein ...
+];
+function logout() {
+  alert("Logged out!");
+  window.location.href = "login.html";
+}
+
